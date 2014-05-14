a0959275607441e5e2c41cc27130ba6ee377c53e
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2014-05-14 06:19:27 -0700

    Added 2 minute timeout limitation to prevent accidentally DDoS'ing a server

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timedelta
 from bingobot import BingoBot
 from botcommands import allCommands
+
+TWO_MINUTES = timedelta(minutes=2)
 
 server = "irc2.speedrunslive.com"
 channel = "#bingoleague"
@@ -7,8 +10,11 @@ botnick = "BingoBot"
 
 bingoBot = BingoBot(botnick, server, channel, commands = allCommands)
 
+lastConnection = datetime(year=1999, month=1, day=1)
+
 # infinite loop tries to reconnect if disconnected by timeout
-while True:
+while datetime.now() - lastConnection > TWO_MINUTES:
+    lastConnection = datetime.now()
     bingoBot.connect()
     bingoBot.listen()
     
