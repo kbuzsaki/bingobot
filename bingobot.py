b2543ceaa379af5868c685601c2a8816f14f0aec
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2014-05-21 13:39:37 -0700

    Added message queue to deal with multiple messages being received at once

--- bingobot.py
+++ bingobot.py
@@ -1,6 +1,7 @@
 import socket
 import traceback
 import re
+from collections import deque
 from datetime import timedelta
 from termcolor import colored, cprint
 from messages import Message, isMessage
@@ -72,6 +73,7 @@ class BingoBot:
         self.server = server
         self.channels = channels
         self.commands = builtinCommands + commands
+        self.messageQueue = deque()
         self.racers = dict()
 
     def send(self, s):
@@ -96,32 +98,41 @@ class BingoBot:
 
     def listen(self):
         while True:
-            ircmsg = self.ircsock.recv(2048)
-            ircmsg = ircmsg.decode("latin-1").strip()
-            # pings are blue
-            if isPing(ircmsg):
-                print(colored(ircmsg, "blue"))
-                pingmsg = ircmsg.split("PING :")[1]
-                self.send("PONG :" + pingmsg + "\n")
-            # chat messages are grey
-            elif isMessage(ircmsg):
-                print(ircmsg)
-                self.processMessage(ircmsg)
-            # if there's SOMETHING there
-            elif len(ircmsg) > 0:
-                print(colored(ircmsg, "green"))
-            # else, must be disconnected
+            # if messages are available, process them
+            if(len(self.messageQueue) > 0):
+                ircmsg = self.messageQueue.popleft()
+                self.processLine(ircmsg)
+            # otherwise, wait for a new batch of messages
             else:
-                print(colored("Disconnected from server?", "yellow"))
-                return
+                ircmsg = self.ircsock.recv(2048)
+                ircmsg = ircmsg.decode("latin-1").strip()
+                self.messageQueue.extend(ircmsg.split("\n"))
 
-            # weird hack thing for joining channels?
-            if "End of /MOTD" in ircmsg:
-                for channel in self.channels:
-                    self.joinchan(channel)
-            # weird hack thing for nickserv identify
-            if "NickServ" in ircmsg and "/msg NickServ IDENTIFY" in ircmsg:
-                self.sendmsg("NickServ", "IDENTIFY " + self.password)
+    def processLine(self, ircmsg):
+        # pings are blue
+        if isPing(ircmsg):
+            print(colored(ircmsg, "blue"))
+            pingmsg = ircmsg.split("PING :")[1]
+            self.send("PONG :" + pingmsg + "\n")
+        # chat messages are grey
+        elif isMessage(ircmsg):
+            print(ircmsg)
+            self.processMessage(ircmsg)
+        # if there's SOMETHING there
+        elif len(ircmsg) > 0:
+            print(colored(ircmsg, "green"))
+        # else, must be disconnected
+        else:
+            print(colored("Disconnected from server?", "yellow"))
+            return
+
+        # weird hack thing for joining channels?
+        if "End of /MOTD" in ircmsg:
+            for channel in self.channels:
+                self.joinchan(channel)
+        # weird hack thing for nickserv identify
+        if "NickServ" in ircmsg and "/msg NickServ IDENTIFY" in ircmsg:
+            self.sendmsg("NickServ", "IDENTIFY " + self.password)
 
     def processMessage(self, ircmsg):
         msg = Message(ircmsg)
