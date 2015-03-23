cc74913b27ea790728027c5ea361948e85670f59
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2015-03-23 00:06:55 -0700

    added more sophisticated duplicate and overlap detection

--- racecommands.py
+++ racecommands.py
@@ -70,6 +70,157 @@ class BingoGenerator:
                 return seed, card
         raise Exception("Failed to generate card for teamsize: " + str(teamSize))
 
+# list of groups goals which cannot be completed on the same file
+# prevent more of these from being on a card than their are players
+EXCLUSIVE_GOALS_LIST = [
+    [
+        'Green Gauntlets',
+        'Golden Gauntlets',
+        'Silver Gauntlets',
+        'Bronze Gauntlets',
+        'Blue Gauntlets',
+        'Goron Bracelet'
+    ]
+]
+
+OVERLAP_GOALS_LIST = [
+    [
+        '3 unused keys in Gerudo Training Grounds',
+        '4 unused keys in Gerudo Training Grounds',
+        '5 unused keys in Gerudo Training Grounds',
+        '6 unused keys in Gerudo Training Grounds',
+        '8 different unused keys in Gerudo Training Grounds',
+    ],
+    [
+        '6 Compasses',
+        '7 Compasses',
+    ],
+    [
+        '6 Maps',
+        '7 Maps',
+    ],
+    [
+        '6 Hearts',
+        '7 Hearts',
+        '8 Hearts',
+        '9 Hearts',
+    ],
+    [
+        'Bullet Bag (40)',
+        'Bullet Bag (50)',
+    ],
+    [
+        'Quiver (40)',
+        'Quiver (50)',
+    ],
+    [
+        'At least 3 songs',
+        'At least 4 songs',
+        'At least 6 songs',
+        'At least 8 songs',
+        'At least 9 songs',
+    ],
+    [
+        'Defeat Queen Gohma',
+        'Beat the Deku Tree',
+    ],
+    [
+        'Defeat King Dodongo',
+        "Beat Dodongo's Cavern",
+    ],
+    [
+        'Defeat Barinade',
+        "Beat Jabu-Jabu's Belly",
+    ],
+    [
+        'Defeat Phantom Ganon',
+        'Beat the Forest Temple',
+    ],
+    [
+        'Defeat Volvagia',
+        'Beat the Fire Temple',
+    ],
+    [
+        'Defeat Morpha',
+        'Beat the Water Temple',
+    ],
+    [
+        'Defeat Bongo-Bongo',
+        'Beat the Shadow Temple',
+    ],
+    [
+        'Defeat Nabooru-Knuckle',
+        'Defeat Twinrova',
+        'Beat the Spirit Temple',
+    ],
+    [
+        'At least 3 Skulltulas in Water Temple',
+        'All 5 Skulltulas in Water Temple',
+    ],
+    [
+        'At least 4 Skulltulas in Shadow Temple',
+        'All 5 Skulltulas in Shadow Temple',
+    ],
+    [
+        'Goron Tunic',
+        'Zora Tunic',
+        '3 Tunics',
+        '3 Swords & 3 Tunics',
+        '3 Tunics & 3 Boots',
+        '3 Shields & 3 Tunics',
+    ],
+    [
+        'Iron Boots',
+        '3 Boots',
+        '3 Shields & 3 Boots',
+        '3 Swords & 3 Boots',
+        '3 Tunics & 3 Boots',
+    ],
+    [
+        'Mirror Shield',
+        '3 Swords & 3 Shields',
+        '3 Shields & 3 Boots',
+        '3 Shields & 3 Tunics',
+    ],
+    [
+        "Giant's Knife",
+        '3 Swords & 3 Tunics',
+        '3 Swords & 3 Boots',
+        '3 Swords & 3 Shields',
+    ],
+    [
+        'At least 7 Magic Beans',
+        'At least 9 Magic Beans',
+    ],
+    [
+        'Fairy Bow',
+        'Defeat Amy (Green Poe)',
+        'Defeat Meg (purple Poe)',
+    ],
+    [
+        'Fairy Bow',
+        'Forest Temple Boss Key',
+    ],
+    [
+        'Fairy Bow',
+        'Fire Arrow',
+    ],
+    [
+        "Giant's Wallet",
+        '500 Rupees',
+    ],
+    [
+        'Defeat Dark Link',
+        'Longshot',
+    ],
+    [
+        'Fire Temple Boss Key',
+        'Free all 9 gorons in Fire Temple',
+    ]
+]
+
+def getInstanceCount(card, goals):
+    return len([goal for goal in card if goal in goals])
 
 def isBlackoutCard(card, teamSize=3):
     names = [goal["name"] for goal in card]
@@ -77,6 +228,16 @@ def isBlackoutCard(card, teamSize=3):
     # no duplicates allowed
     if len(names) != len(set(names)):
         return False
+
+    for exclusiveGoals in EXCLUSIVE_GOALS_LIST:
+        count = getInstanceCount(names, exclusiveGoals)
+        if count > teamSize:
+            return False
+
+    for overlapGoals in OVERLAP_GOALS_LIST:
+        count = getInstanceCount(names, overlapGoals)
+        if count > 1:
+            return False
 
     return True
 
