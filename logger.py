f493b2e8e6877dca9d0e6abfeb877ac28dc28a47
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2015-09-11 18:31:18 -0700

    Added simple priority threshold to logger

--- logger.py
+++ logger.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from enum import IntEnum, unique
 
 from termcolor import colored
 
@@ -8,19 +9,28 @@ INCOMING_INFO_COLOR = "green"
 INCOMING_PING_COLOR = "blue"
 OUTGOING_COLOR = "magenta"
 
+@unique
+class Priority(IntEnum):
+    verbose = 1
+    default = 2
+    error = 3
 
 class Logger:
     """Basic colorizing logger. Prints to terminal by default."""
 
-    def _log(self, message, color=None):
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(colored(now + " - " + message, color))
+    def __init__(self):
+        self.threshold = Priority.verbose
+
+    def _log(self, message, color=None, priority=Priority.default):
+        if priority >= self.threshold:
+            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(colored(now + " - " + message, color))
 
     def log(self, message):
-        self._log(message)
+        self._log(message, priority=Priority.verbose)
 
     def error(self, message, e=None):
-        self._log(message, ERROR_COLOR)
+        self._log(message, ERROR_COLOR, priority=Priority.error)
 
     def debug(self, message):
         self._log(message, DEBUG_COLOR)
@@ -32,6 +42,6 @@ class Logger:
         self._log(message, INCOMING_PING_COLOR)
 
     def outgoing(self, message):
-        self._log(message, OUTGOING_COLOR)
+        self._log(message, OUTGOING_COLOR, priority=Priority.verbose)
 
 logger = Logger()
